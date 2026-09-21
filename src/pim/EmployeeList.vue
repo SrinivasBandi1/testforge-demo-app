9100e903e7c62222081ff4d73791419aa6f64951
SrinivasBandi1 <134129477+SrinivasBandi1@users.noreply.github.com>
2026-09-21 10:07:02 +0530

    Create EmployeeList.vue

---
+++ src/pim/EmployeeList.vue
@@ -0,0 +1,9 @@
+<template>
+  <div class="employee-list">
+    <h1>Employee List</h1>
+    <button class="btn-primary">Add Employee</button>
+    <input placeholder="Employee Name" />
+    <button class="btn-secondary">Search</button>
+    <p class="records">Records found</p>
+  </div>
+</template>
